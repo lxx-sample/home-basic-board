b116c13e9de516199513cc5639b9e427a3804689
dongpo.li <dongpo.li@hotmail.com>
2021-06-18 18:47:24 +0800

    改成表格的形式

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -16,12 +16,22 @@ export class AppComponent {
     })
   });
 
-  calc = new Promise<string[]>((resolve, reject) => {
+  panel = new Observable<string>(observer => {
+    // setInterval(() => observer.next(new Date().toString()), 1000);
+
+    MessageHandlerManager.registerHandler('/system/handler/panel', (args: Map<string, any>) => {
+      // resolve(args.get('panel'))
+      observer.next(args.get('panel'));
+    })
+
+  });
+
+  calc = new Promise<Map<string, string>[]>((resolve, reject) => {
     MessageHandlerManager.registerHandler('/fund/trade/helper/calc', function (args: Map<string, any>) {
       var validlyTradeList = args.get('validlyTradeList');
       var latestPrice = args.get('latestPrice');
 
-      let helper: string[] = [];
+      let helper: Map<string, string>[] = [];
 
       Object.keys(validlyTradeList).forEach(k => {
         let element = validlyTradeList[k];
@@ -29,7 +39,15 @@ export class AppComponent {
         let message = '最新净值: ' + latestPrice + ', 确认净值: ' + element['price'] + ', 持有份额: ' + element['tradeNumber'];
         message += ', 持有天数: ' + element['days'] + ', 收益率: ' + element['yield'];
 
-        helper.push(message)
+        let each = new Map();
+        each.set("latestPrice", latestPrice);
+        each.set("price", element['price']);
+        each.set("tradeNumber", element['tradeNumber']);
+        each.set("days", element['days']);
+        each.set("yield", element['yield']);
+        each.set("totalAmount", (element['price'] * element['tradeNumber']).toFixed(2));
+
+        helper.push(each)
       });
 
       resolve(helper)
