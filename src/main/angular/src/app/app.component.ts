b5e1d7a64cb621e047fad26579abcd21259c4db6
dongpo.li <dongpo.li@hotmail.com>
2021-06-19 15:51:03 +0800

    重构前端的handler

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subscriber } from 'rxjs';
+import { stringify } from '@angular/compiler/src/util';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Observable, of, Subscriber } from 'rxjs';
 
 import { MessageHandlerManager } from './tools/websocket.component'
 
@@ -10,50 +11,72 @@ import { MessageHandlerManager } from './tools/websocket.component'
 })
 export class AppComponent {
 
-  uuid = new Promise((resolve, reject) => {
-    MessageHandlerManager.registerHandler('/system/handler/uuid', (args: Map<string, any>) => {
-      resolve(args.get('uuid'))
+  uuid: string = "";
+  panel: string = "";
+  fundTradeHistoryList: Map<string, string>[] = [];
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    this.registerDeviceUuidHandler();
+    this.registerPanelChangeHandler();
+    this.registerFundTradeHistoryHandler();
+  }
+
+  registerPanelChangeHandler() {
+    let subject = MessageHandlerManager.getSubject('/system/handler/panel');
+    subject?.subscribe(args => {
+      this.panel = <string>args.get('panel');
+      this.changeDetectorRef.detectChanges();
     })
-  });
+  }
 
-  panel = new Observable<string>(observer => {
-    // setInterval(() => observer.next(new Date().toString()), 1000);
-
-    MessageHandlerManager.registerHandler('/system/handler/panel', (args: Map<string, any>) => {
-      // resolve(args.get('panel'))
-      observer.next(args.get('panel'));
+  registerDeviceUuidHandler() {
+    let subject = MessageHandlerManager.getSubject('/system/handler/uuid');
+    subject?.subscribe(args => {
+      this.uuid = <string>args.get('uuid');
+      this.changeDetectorRef.detectChanges();
     })
+  }
 
-  });
+  registerFundTradeHistoryHandler() {
+    let fundTradeHistoryChangeSubject = MessageHandlerManager.getSubject('/fund/trade/helper/calc');
+    fundTradeHistoryChangeSubject?.subscribe(args => {
 
-  calc = new Promise<Map<string, string>[]>((resolve, reject) => {
-    MessageHandlerManager.registerHandler('/fund/trade/helper/calc', function (args: Map<string, any>) {
       var validlyTradeList = args.get('validlyTradeList');
       var latestPrice = args.get('latestPrice');
-
       let helper: Map<string, string>[] = [];
 
       Object.keys(validlyTradeList).forEach(k => {
         let element = validlyTradeList[k];
 
-        let message = '最新净值: ' + latestPrice + ', 确认净值: ' + element['price'] + ', 持有份额: ' + element['tradeNumber'];
-        message += ', 持有天数: ' + element['days'] + ', 收益率: ' + element['yield'];
-
         let each = new Map();
+        // 最新净值
         each.set("latestPrice", latestPrice);
+        // 确认净值
         each.set("price", element['price']);
+        // 持有份额
         each.set("tradeNumber", element['tradeNumber']);
+        // 持有天数
         each.set("days", element['days']);
-        each.set("yield", element['yield']);
-        each.set("totalAmount", (element['price'] * element['tradeNumber']).toFixed(2));
+        // 收益率
+        let _yield = element['yield'];
+        each.set("yield", _yield);
+        if (parseFloat(_yield) > 0) {
+          each.set("bg", "red");
+        } else {
+          each.set("bg", "green");
+        }
+        // 交易金额
+        let totalAmount = (element['price'] * element['tradeNumber']).toFixed(2);
+        each.set("totalAmount", totalAmount);
+        
 
         helper.push(each)
       });
 
-      resolve(helper)
+      this.fundTradeHistoryList = helper;
+      this.changeDetectorRef.detectChanges();
     })
-
-  });
+  }
 
   background = "lightgreen"
 }
--- src/main/angular/src/app/tools/websocket.component.ts
+++ src/main/angular/src/app/tools/websocket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
 
 (function () {
   let ws = new WebSocket('ws://localhost:18080/web/socket/dashboard');
@@ -23,10 +24,10 @@ import { Component, OnInit } from "@angular/core";
 
     if (messageObject && messageObject['path']) {
 
-      let handler = messageObject['path'];
+      let path = messageObject['path'];
 
-      let action = MessageHandlerManager.get(handler);
-      if (action) {
+      let subject = MessageHandlerManager.getSubject(path);
+      if (subject) {
         let map = new Map();
         if (messageObject['args']) {
           for (let k of Object.keys(messageObject['args'])) {
@@ -34,7 +35,7 @@ import { Component, OnInit } from "@angular/core";
           }
         }
 
-        action(map);
+        subject.next(map)
       }
     }
 
@@ -47,22 +48,17 @@ import { Component, OnInit } from "@angular/core";
 
 export class MessageHandlerManager {
 
-  private static MESSAGE_HANDLER_MAPPING: Map<string, Function> = new Map();
+  private static MESSAGE_HANDLER_MAPPING: Map<string, Subject<Map<string, any>>> = new Map();
 
-  public static registerHandler = function (handler: string, action: Function) {
-    if (MessageHandlerManager.MESSAGE_HANDLER_MAPPING.get(handler)) {
-      throw 'handler ' + handler + ' already exists';
+  public static getSubject = function (path: string) {
+    if (MessageHandlerManager.MESSAGE_HANDLER_MAPPING.get(path)) {
+      return MessageHandlerManager.MESSAGE_HANDLER_MAPPING.get(path);
     }
 
-    MessageHandlerManager.MESSAGE_HANDLER_MAPPING.set(handler, action);
-  }
+    let subject = new Subject<Map<string, any>>();
+    MessageHandlerManager.MESSAGE_HANDLER_MAPPING.set(path, subject);
 
-  public static get = function (handler: string) {
-    if (MessageHandlerManager.MESSAGE_HANDLER_MAPPING.get(handler)) {
-      return MessageHandlerManager.MESSAGE_HANDLER_MAPPING.get(handler);
-    }
-
-    throw 'no handler ' + handler + ' exists';
+    return subject;
   }
 
 }
