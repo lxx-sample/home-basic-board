9d7b76005b42e011b1312f58aaf512b527ee1b5f
dongpo.li <dongpo.li@hotmail.com>
2022-02-18 11:35:38 +0800

    build脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-./gradlew build -x test
+cd /Users/dongpo.li/IdeaProjects_workspace/home-basic-board/src/main/angular && npm run build
 
-#scp /Users/dongpo.li/IdeaProjects_workspace/home-basic-board/build/libs/home-basic-board-1.0.0.jar dongpo.li@39.105.88.127:/home/dongpo.li/home-basic-board/
+cd /Users/dongpo.li/IdeaProjects_workspace/home-basic-board && ./gradlew build -x test
+
+scp /Users/dongpo.li/IdeaProjects_workspace/home-basic-board/build/libs/home-basic-board-1.0.0.jar dongpo.li@39.105.88.127:/home/dongpo.li/home-basic-board/
 
